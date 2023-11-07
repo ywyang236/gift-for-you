25abf1147e5ef78703b60b3746d06e53afbe9c69
ywyang236 <ywyang236@gmail.com>
2023-11-07 22:30:41 +0800

    Change favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,17 +6,17 @@ import Footer from '../components/Footer';
 import './globals.css';
 
 export default function Layout({children}: {children: React.ReactNode}) {
-	return (
-		<>
-			<Head>
-				<title>禮品訂製所 Gift For You</title>
-				<meta name='description' content='親手繪製您獨一無二的紀念品。' />
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<Navbar />
-			<main>{children}</main>
-			<Footer />
-		</>
-	);
+    return (
+        <>
+            <Head>
+                <title>禮品訂製所 Gift For You</title>
+                <meta name='description' content='親手繪製您獨一無二的紀念品。' />
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+        </>
+    );
 }
