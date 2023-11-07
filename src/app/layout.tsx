dc00323d18f6213e497775cf735b4e9bf7e1834c
ywyang236 <ywyang236@gmail.com>
2023-11-08 00:45:27 +0800

    Update navigation bar and add main visual image to homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({children}: {children: React.ReactNode}) {
         <>
             <Head>
                 <title>禮品訂製所 Gift For You</title>
-                <meta name='description' content='親手繪製您獨一無二的紀念品。' />
+                <meta name='description' content='親手繪製您獨一無二的紀念品' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 // pages/index.tsx
 import React from 'react';
 import Layout from '../app/layout';
+import IndexCSS from '../styles/index.module.css';
 
 const Home = () => (
-	<Layout>
-		<a></a>
-	</Layout>
+    <Layout>
+        <div className={IndexCSS.main}>
+            <div className={IndexCSS.container}>
+                <span className={IndexCSS.title}>禮品訂製所</span>
+                <span className={IndexCSS.description}>親手繪製您獨一無二的紀念品</span>
+
+            </div>
+        </div>
+    </Layout >
 );
 
 export default Home;
