dd353f28fa9591555d075f590d271acc92315f0b
ywyang236 <ywyang236@gmail.com>
2023-11-09 05:19:39 +0800

    Add design page structure and styling.

---
+++ src/components/CanvasNavbar/CanvasNavbar.tsx
@@ -0,0 +1,12 @@
+// components/CanvasNavbar/CanvasNavbar.tsx
+import React from "react";
+import CanvasNavbarCSS from "./CanvasNavbar.module.css";
+
+const CanvasNavbar: React.FC = () => (
+    <nav className={CanvasNavbarCSS.main}>
+        <div className={CanvasNavbarCSS.container}>
+        </div>
+    </nav>
+);
+
+export default CanvasNavbar;
--- src/pages/start-design.tsx
+++ src/pages/start-design.tsx
@@ -1,11 +1,25 @@
 // pages/design-gift.tsx
 import React from 'react';
 import Layout from '../app/layout';
+import DesignCSS from '../styles/design.module.css';
+
 
 const DesignGift = () => (
     <Layout>
-        <h1>禮品</h1>
-        {/* Canvas 編輯器組件 */}
+        <div className={DesignCSS.main}>
+            <div className={DesignCSS.container}>
+                <div className={DesignCSS.designNavbar}>
+                    <div className={DesignCSS.designTitle}>幾何造型夜燈</div>
+                    <div className={DesignCSS.designButton}></div>
+                </div>
+                <div className={DesignCSS.designDown}>
+                    <div className={DesignCSS.designCanvas}>
+                        <div className={DesignCSS.designItem}></div>
+                    </div>
+                    <div className={DesignCSS.designTools}></div>
+                </div>
+            </div>
+        </div>
     </Layout>
 );
 
