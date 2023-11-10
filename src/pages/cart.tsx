c339402bad6adb4522812a7fb864108cff7c484d
ywyang236 <ywyang236@gmail.com>
2023-11-10 15:17:13 +0800

    Add cart page

---
+++ src/pages/cart.tsx
@@ -0,0 +1,11 @@
+// pages/cart.tsx
+import React from 'react';
+import Layout from '../app/layout';
+
+const Cart = () => (
+    <Layout>
+        <h1>購物車</h1>
+    </Layout>
+);
+
+export default Cart;
