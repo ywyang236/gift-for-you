927ca627279412db6776415619322e3ddaf42d5a
ywyang236 <ywyang236@gmail.com>
2023-11-23 21:36:00 +0800

    Update footer text.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import FooterCSS from './Footer.module.css';
 
 const Footer: React.FC = () => (
     <footer className={FooterCSS.footer}>
-        <p>版權 © 2023 禮品訂製所 Gift For You。保留所有權利。</p>
+        <p>版權 © 2023 禮品訂製所 Gift For You</p>
     </footer>
 );
 
