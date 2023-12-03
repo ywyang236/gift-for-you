af97b4e9e89b44e72ef950ca39c55cf4c0c85d6d
ywyang236 <ywyang236@gmail.com>
2023-12-04 03:40:23 +0800

    Update member page background.

--- src/pages/member-information.tsx
+++ src/pages/member-information.tsx
@@ -6,8 +6,10 @@ import MemberCSS from '../styles/member.module.css';
 const MemberInformation = () => (
     <Layout>
         <div className={MemberCSS.main}>
-            <div className={MemberCSS.container}>
-                <h1>會員資料</h1>
+            <div className={MemberCSS.backgroundContainer}>
+                <div className={MemberCSS.container}>
+                    <h1>會員資料</h1>
+                </div>
             </div>
         </div>
     </Layout>
