2e929c71da26030ee3ba5ec549a7e70be6ef50b1
ywyang236 <ywyang236@gmail.com>
2024-01-07 06:45:07 +0800

    Add about author page discription.

--- src/pages/about-author.tsx
+++ src/pages/about-author.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 // pages/about-author.tsx
 import React from 'react';
 import Layout from '../app/layout';
@@ -8,7 +9,14 @@ const AboutAuthor = () => (
         <div className={AboutAuthorCSS.main}>
             <div className={AboutAuthorCSS.backgroundContainer}>
                 <div className={AboutAuthorCSS.container}>
-                    <h1>關於作者</h1>
+                    <h1 className={AboutAuthorCSS.title}>About Author</h1>
+                    <div></div>
+                    <p className={AboutAuthorCSS.text}>
+                        My name is Yu-Wei Yang (楊于葳), a Trainee of the fourth cohort at WeHelp Bootcamp, where I embarked on a six-month journey of web development training from July to December 2023. Before joining WeHelp, I had only just grasped the basics of JavaScript and had never worked with Python. As I joined the Bootcamp, I began to massively immerse myself in unfamiliar languages and technologies, tailoring my learning to my own pace and completing the weekly assignments.
+                    </p>
+                    <p className={AboutAuthorCSS.text}>
+                        During this period, I dedicated seven weeks to independently develop a full-stack e-commerce site called 'Taipei Day Trip' strictly following the specification documents. Additionally, 'Gift For You' is a drawing e-commerce site that I developed independently over six weeks, without following any guidelines. These projects were significant milestones in my journey, demonstrating my ability to learn rapidly and apply new skills in practical, project-based environments.
+                    </p>
                 </div>
             </div>
         </div>
