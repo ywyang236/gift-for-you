18b164db9143676fa5eba93973866981a8e651d7
ywyang236 <ywyang236@gmail.com>
2024-01-11 08:03:35 +0800

    Try to find deploy fail problems.

--- jest.config.ts
+++ jest.config.ts
@@ -9,7 +9,7 @@ const config: Config.InitialOptions = {
     },
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
-        '\\.css$': '<rootDir>/src/emptyModule.js',
+        '\\.css$': '<rootDir>/emptyModule.js',
         '\\.module\\.css$': 'identity-obj-proxy',
     },
 };
